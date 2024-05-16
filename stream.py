4fafea7e7c3d9ec0e3d787050f8c34a96ceecc7a
eyradel <60956082+eyradel@users.noreply.github.com>
2024-05-16 18:45:16 +0000

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,4 +1,9 @@
 import streamlit as st 
+import os
+import io
+from google.cloud import vision
+from googletrans import Translator
+import time
 
 st.markdown(
     '<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">',
@@ -26,10 +31,75 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.markdown(
     """
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #4267B2;">
-    <a class="navbar-brand" href="#"  target="_blank">GERMAN OCR</a>  
+    <a class="navbar-brand" href="#"  target="_blank">Redox OCR</a>  
     </nav>
 """,
     unsafe_allow_html=True,
 )
 
 st.markdown(f"<div class ='card alert alert-success' style='color:black'>Optical Character recognition Software</div>",unsafe_allow_html=True)
+
+
+
+
+file_upload = st.file_uploader("Upload Image file")
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials.json'
+
+def detect_text(image_path):
+    """Detects text in an image using Google Vision API."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+    start_time = time.time()
+    response = client.text_detection(image=image)
+    end_time = time.time()
+    texts = response.text_annotations
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    
+    if texts:
+        return texts[0].description, end_time - start_time
+    else:
+        return None, end_time - start_time
+
+def translate_text(text, src='de', dest='en'):
+    """Translates text from source language to destination language using Google Translate API."""
+    translator = Translator()
+    translation = translator.translate(text, src=src, dest=dest)
+    return translation.text
+
+def detect_and_translate(image_path):
+    """Detects text in an image and translates it from German to English."""
+    german_text, detection_time = detect_text(image_path)
+    with st.expander("Original Image"):
+        st.image(file_upload.name)
+
+    if german_text:
+
+        with st.expander("Metrics"):
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Detection Time", detection_time, "secs")
+ 
+        with st.expander("German Text"):
+            st.markdown(f"<span class='note card alert-warning ' style='color: black'>{german_text}</span>",unsafe_allow_html=True)
+        
+        print("")
+        english_text = translate_text(german_text)
+        with st.expander("Translated Text"):
+            st.markdown(f"<span class='note card' style='color: black'>{english_text}</span>",unsafe_allow_html=True)
+        
+    else:
+        st.write("No text detected.")
+
+
+if st.button("submit"):
+    with st.spinner("Loading..."):
+        detect_and_translate(file_upload.name)
